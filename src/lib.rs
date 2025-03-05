1bdc4a325071b7f942859647df97fdef0a0fd8e7
waseemR02 <waseem.riaz.9999@gmail.com>
2025-03-05 07:49:55 +0530

    Add image module and tests for RGB image manipulation

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,43 @@
 //! encode messages into images and decode them back without visible changes to the image.
 
 mod bltm;
-mod encoder;
 mod decoder;
+mod encoder;
 mod error;
+mod img;
 mod utils;
+
+// Re-export items for public API
+pub use error::HideError;
+pub use img::{create_rgb_image, load_image, save_image, StegoImage};
+// Re-export the Rgb type from the image crate for convenient use
+pub use image::Rgb;
 
 /// The result type returned by functions in this library.
 pub type Result<T> = std::result::Result<T, error::HideError>;
+
+/// Version of the hide-rs library
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_image_operations() {
+        // Create a small test image
+        let mut img = create_rgb_image(2, 2);
+
+        // Set some pixel values
+        img.set_pixel_rgb(0, 0, Rgb([255, 0, 0])).unwrap();
+        img.set_pixel_rgb(1, 0, Rgb([0, 255, 0])).unwrap();
+        img.set_pixel_rgb(0, 1, Rgb([0, 0, 255])).unwrap();
+        img.set_pixel_rgb(1, 1, Rgb([255, 255, 255])).unwrap();
+
+        // Verify pixel values
+        assert_eq!(img.get_pixel_rgb(0, 0).unwrap().0, [255, 0, 0]);
+        assert_eq!(img.get_pixel_rgb(1, 0).unwrap().0, [0, 255, 0]);
+        assert_eq!(img.get_pixel_rgb(0, 1).unwrap().0, [0, 0, 255]);
+        assert_eq!(img.get_pixel_rgb(1, 1).unwrap().0, [255, 255, 255]);
+    }
+}
