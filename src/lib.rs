2b8e9780ba83210bccab37131b3c1a44ce8f1e36
waseemR02 <waseem.riaz.9999@gmail.com>
2025-03-05 08:03:59 +0530

    Refactor image module imports and update pixel setting in tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,8 @@ mod bltm;
 mod decoder;
 mod encoder;
 mod error;
-mod img;
-mod utils;
-
-// Re-export items for public API
-pub use error::HideError;
-pub use img::{create_rgb_image, load_image, save_image, StegoImage};
-// Re-export the Rgb type from the image crate for convenient use
-pub use image::Rgb;
+pub mod img;
+pub mod utils;
 
 /// The result type returned by functions in this library.
 pub type Result<T> = std::result::Result<T, error::HideError>;
@@ -30,13 +24,14 @@ mod tests {
     #[test]
     fn test_image_operations() {
         // Create a small test image
-        let mut img = create_rgb_image(2, 2);
+        let mut img = img::create_rgb_image(2, 2);
 
         // Set some pixel values
-        img.set_pixel_rgb(0, 0, Rgb([255, 0, 0])).unwrap();
-        img.set_pixel_rgb(1, 0, Rgb([0, 255, 0])).unwrap();
-        img.set_pixel_rgb(0, 1, Rgb([0, 0, 255])).unwrap();
-        img.set_pixel_rgb(1, 1, Rgb([255, 255, 255])).unwrap();
+        img.set_pixel_rgb(0, 0, image::Rgb([255, 0, 0])).unwrap();
+        img.set_pixel_rgb(1, 0, image::Rgb([0, 255, 0])).unwrap();
+        img.set_pixel_rgb(0, 1, image::Rgb([0, 0, 255])).unwrap();
+        img.set_pixel_rgb(1, 1, image::Rgb([255, 255, 255]))
+            .unwrap();
 
         // Verify pixel values
         assert_eq!(img.get_pixel_rgb(0, 0).unwrap().0, [255, 0, 0]);
